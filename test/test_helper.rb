7a10a3baa7671657006f0e0c62522005401759d9
Matt Olson <matt@mattolson.com>
2013-04-10 22:07:38 -0700

    after after_bundler for minitest_capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
+require 'capybara/rails'
 require 'pry-rescue/minitest'
 require 'minitest/matchers'
 require 'minitest/metadata'
@@ -11,6 +12,16 @@ Dir[File.expand_path('test/support/*.rb')].each { |file| require file }
 
 # Setup class for integration tests
 class IntegrationTest < MiniTest::Spec
+
+  before do
+    if metadata[:js]
+      Capybara.current_driver = :webkit
+    else
+      Capybara.current_driver = Capybara.default_driver
+    end
+  end
+
+  include Capybara::DSL
   include Devise::TestHelpers
   include Rails.application.routes.url_helpers
   include MiniTest::Metadata
@@ -28,4 +39,14 @@ end
 class UploaderTest < MiniTest::Spec
   include ActionDispatch::TestProcess
   register_spec_type(/Uploader$/, self)
+end
+
+# Transactional fixtures do not work with Selenium tests, because Capybara
+# uses a separate server thread, which the transactions would be hidden
+# from. We hence use DatabaseCleaner to truncate our test database.
+DatabaseCleaner.strategy = :truncation
+class MiniTest::Spec
+  before :each do
+    DatabaseCleaner.clean
+  end
 end
