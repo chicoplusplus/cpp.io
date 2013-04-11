236452c31472a5880bc591a77633a8f693871a94
Matt Olson <matt@mattolson.com>
2013-04-10 22:05:07 -0700

    after before_everything for factory_girl

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,7 +2,7 @@ Rails.application.config.generators do |g|
   g.orm :active_record
   g.template_engine :erb
   g.test_framework nil # Using minitest, but don't want generators
-  g.fixture_replacement nil
+  g.fixture_replacement :factory_girl, :dir => 'test/factories'
   g.scaffold_controller "scaffold_controller"
   g.stylesheets true
   g.javascripts true
