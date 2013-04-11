27bce07dd04418652934b4af7c5188f470e7f5df
Matt Olson <matt@mattolson.com>
2013-04-10 22:03:18 -0700

    Fresh rails app before customizations.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chicoplusplus::Application.config.secret_token = 'e8ed157b734860a99ba5681569ddacafe0e388a1311e7d4b140642fcafcf153da868863a92e20eb33ed9dd65c2b2c24ee0501849090d6f0a9fc56732ee0cf87a'
