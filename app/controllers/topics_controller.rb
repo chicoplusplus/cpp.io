dbffc685489faf3edfd1331a06f027634879960b
Matt Olson <matt@mattolson.com>
2013-04-17 17:18:45 -0700

    Skip authorization for voting

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,6 @@
 class TopicsController < ApplicationController
   # Cancan authorization
-  skip_authorization_check :only => [:vote]
-  authorize_resource
+  authorize_resource :except => [:vote]
 
   # GET /topics
   # GET /topics.json
